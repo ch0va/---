0142d0d204c0a3ffe90abc80367881c70c093bbd
ch0va <ch0va@mail.ru>
2023-03-06 23:55:31 +0300

    hide mobile menu

--- Metrics/Scripts/script.js
+++ Metrics/Scripts/script.js
@@ -1,15 +1,14 @@
 const mobileHeader = document.getElementById("mobile-header");
 const blackout = document.getElementById("blackout");
 const cbMenu = document.querySelector("#toggle input");
-const body = document.getElementById('body');
-const links= document.querySelectorAll('#mobile-header  a');
+const body = document.getElementById("body");
+const links = document.querySelectorAll("#mobile-header  a");
 links.forEach((link) => {
   link.onclick = function () {
     cbMenu.checked = false;
     toggle.click();
   };
 });
-
 
 toggle.addEventListener("click", function () {
   mobileHeader.classList.toggle("active");
@@ -22,12 +21,3 @@ blackout.onclick = function () {
   toggle.click();
   menuOn = false;
 };
-
-
-
-// if (cbMenu.checked === false) {
-//   window.addEventListener("scroll", function () {
-//     cbMenu.checked = false;
-//     toggle.click();
-//   });
-// }
